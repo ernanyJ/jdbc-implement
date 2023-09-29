1f579ebf1bd4a7aa7b7f0c156837db37c2a64161
ernanyJ <julioernany@gmail.com>
2023-09-29 03:07:31 -0300

    implemented all method tests in main class

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
 
@@ -15,25 +16,48 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-
-        Date d1 = new Date(2004,06,24);
-
         Department dep = new Department(1,null);
+        Date date = new Date(2005-02-24);
 
+        Seller obj = new Seller(null,"Paulo","paulo@gmail",date,3500.0,dep);
 
-        Seller sel2 = sellerDao.findById(17);
+        System.out.println("\nTEST 1 \n insert obj \n");
+        sellerDao.insert(obj);
+        System.out.println("obj:\n" + obj);
 
-        System.out.println(sel2 + "\n\n");
+        // =====================================================
 
-        sel2.setName("milote");
-        sel2.setEmail("milhote@gmaill");
+        System.out.println("\nTEST 2: \n find by id \n");
+        Seller obj2 = sellerDao.findById(1);
+        System.out.println(obj2);
 
-        System.out.println("updated sel 2\n\n" + sel2 + "\n\n");
+        // =====================================================
 
-        sellerDao.update(sel2);
+        System.out.println("\nTEST 3: \n delete obj \n");
+        System.out.println("deleted seller: " + obj);
+        sellerDao.deleteById(obj.getId());
 
-       // sellerDao.findById(11);
+        // =====================================================
 
+        System.out.println("\nTEST 4: \n find all sellers \n");
+        List<Seller> list = sellerDao.findAll();
+        for(Seller x : list) System.out.println(x);
+
+        // =====================================================
+
+        System.out.println("\nTEST 5: \n update seller \n");
+        Seller obj3 = obj;
+        System.out.println("before changes: " + obj3);
+        obj3.setName("henrique");
+        obj3.setEmail("henrique@gmail.com");
+        System.out.println("\n\nafter changes: " + obj3);
+        sellerDao.update(obj3);
+
+        // =====================================================
+
+        System.out.println("\nTEST 4: \n find by department \n");
+        List<Seller> list2 = sellerDao.findByDepartment(dep);
+        for(Seller x2 : list2) System.out.println(x2);
 
     }
 }
--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,8 +2,11 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program2 {
@@ -11,14 +14,42 @@ public class Program2 {
 
     public static void main(String[] args) {
 
-        DepartmentDao depDao = DaoFactory.createDepartmenDao();
-        Department dep = depDao.findById(12);
 
-        List<Department> list = depDao.findAll();
-        for(Department x : list){
-            System.out.println(x);
-        }
+        Department dep = new Department(null,"Test");
+        DepartmentDao dao = DaoFactory.createDepartmenDao();
 
+
+        System.out.println("\nTEST 1 \n insert obj \n");
+        dao.insert(dep);
+        System.out.println("dep:\n" + dep);
+
+        // =====================================================
+
+        System.out.println("\nTEST 2: \n find by id \n");
+        Department obj = dao.findById(1);
+        System.out.println(obj);
+
+        // =====================================================
+
+        System.out.println("\nTEST 3: \n delete obj \n");
+        System.out.println("deleted department: " + dep);
+        dao.deleteById(dep.getId());
+
+        // =====================================================
+
+        System.out.println("\nTEST 4: \n find all departments \n");
+        List<Department> list = dao.findAll();
+        for(Department x : list) System.out.println(x);
+
+        // =====================================================
+
+        System.out.println("\nTEST 5: \n update department \n");
+        Department dep2 = dep;
+        System.out.println("before changes: " + dep2);
+        dep2.setName("testchange");
+        System.out.println("\n\nafter changes: " + dep2);
+        dao.update(dep2);
+        
     }
 
 
